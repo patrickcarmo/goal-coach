16b1de4409fb90241462681ea234d22b3877277e
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-20 00:02:26 +0100

    SignOut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class App extends Component{
     
+    signOut() {
+        firebaseApp.auth().signOut();
+    }
+
     render(){
-        return(
-            <h1>Goal Coach</h1>
+        return (
+            <div>
+                <h1>Goal Coach</h1>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.signOut()}
+                >
+                    Sign Out
+                </button>
+            </div>
         )
     }
 }
