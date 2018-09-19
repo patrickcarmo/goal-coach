98c0d99f5cca746df3139206d2ea555c6be216f4
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-19 23:45:39 +0100

    SignIn and Link

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { firebaseApp } from '../firebase';
 
 class SignIn extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        };
+    }
+
+    signIn() {
+        console.log('this.state', this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log('error', error);
+                this.setState({ error });
+            })
+    }
+
     render() {
         return (
-            <h1>SignIn</h1>
+            <div className="form-inline">
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="email"
+                        onChange={event => this.setState({ email: event.target.value })}
+                    />
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="password"
+                        onChange={event => this.setState({ password: event.target.value })}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        type="buttton"
+                        onClick={() => this.signIn()}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+                <div><Link to={'/signup'}>Sign up instead</Link></div>
+            </div>
         )
     }
 }
