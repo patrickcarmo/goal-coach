94c00a00892a93dd15aa1b7a916a652b78916c0a
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-09-20 22:13:56 +0100

    GoalList and Combined Reducers

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -15,7 +15,7 @@ class AddGoal extends Component {
     addGoal() {
         console.log('this', this);
         const { title } = this.state;
-        const { email } = this.props;        
+        const { email } = this.props.user;        
         goalRef.push({
             email,
             title
@@ -47,10 +47,9 @@ class AddGoal extends Component {
 }
 
 function mapStateToProps(state) {
-    console.log(state);
-    const { email } = state;
+    const { user } = state;
     return {
-        email
+        user
     };
 }
 
